9fe7db441a28b1b83baab3c64196fcc5d58230ac
hzhangse <2387362072@qq.com>
2015-07-03 23:33:06 +0800

    init

--- src/test/java/cnblogs/jaxb_helloworld/AppTest.java
+++ src/test/java/cnblogs/jaxb_helloworld/AppTest.java
@@ -6,11 +6,11 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import model.ObjectFactory;
-import model.Order;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import model.ObjectFactory;
+import model.Order;
 
 public class AppTest extends TestCase {
 
@@ -32,7 +32,7 @@ public class AppTest extends TestCase {
 	public void testXmlToObj() {
 		JAXBContext jc;
 		try {
-			jc = JAXBContext.newInstance("model");
+			jc = JAXBContext.newInstance("com.train");
 			Unmarshaller u = jc.createUnmarshaller();
 			String xmlFilePath = AppTest.class.getResource("/").getPath()
 					+ "order.xml";
@@ -54,33 +54,33 @@ public class AppTest extends TestCase {
 			ObjectFactory of = new ObjectFactory();
 			Order order = of.createOrder();
 			Order.OrderItems orderItems = of.createOrderOrderItems();
-			
+
 			Order.OrderItems.Produdct product1 = new Order.OrderItems.Produdct();
 			product1.setProductNo("A-01");
 			product1.setProductName("Car");
 			product1.setPrice(1000000);
 			product1.setAmount(1);
-			
+
 			orderItems.getProdudct().add(product1);
-			
+
 			Order.OrderItems.Produdct product2 = new Order.OrderItems.Produdct();
 			product2.setProductNo("B-01");
 			product2.setProductName("Book");
 			product2.setPrice(200);
 			product2.setAmount(2);
-			
+
 			orderItems.getProdudct().add(product2);
-		
+
 			order.setOrderItems(orderItems);
 
-			JAXBContext jc = JAXBContext.newInstance("model");
+			JAXBContext jc = JAXBContext.newInstance("com.train");
 			Marshaller ms = jc.createMarshaller();
 			ms.setProperty("jaxb.encoding", "UTF-8");
 			ms.setProperty("jaxb.formatted.output", true);
 			String xmlFilePath = AppTest.class.getResource("/").getPath()
 					+ "order_new.xml";
 			ms.marshal(order, new File(xmlFilePath));
-			
+
 			System.out.println("testObjToXml");
 		} catch (Exception e) {
 			e.printStackTrace();
